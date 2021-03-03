efe7fe59da4739332b265df9d81d59398d3645b1
Lorran Monteiro <lorranmcm@gmail.com>
2021-03-03 14:51:25 -0300

    Refatorado para OO

--- aquecimento_iugu/study_diary.rb
+++ aquecimento_iugu/study_diary.rb
@@ -1,3 +1,20 @@
+require_relative 'study_item'
+
+def clear
+  system('clear')
+end
+
+def wait_keypress
+  puts
+  puts 'Pressione enter para continuar'
+  gets
+end
+
+def clear_and_wait_keypress
+  wait_keypress
+  clear
+end
+
 def welcome
   'Bem-vindo ao Diário de Estudos, seu companheiro para estudar!'
 end
@@ -17,33 +34,47 @@ def register_study_item
   print 'Digite a categoria do seu item de estudo: '
   category = gets.chomp
   puts "Item '#{title}' da categoria '#{category}' cadastrado com sucesso!"
-  { title: title, category: category }
+  StudyItem.new(title, category)
 end
 
+def print_items(collection)
+  collection.each_with_index do |item, index|
+    puts "##{index + 1} - #{item.title} - #{item.category}"
+  end
+  puts 'Nenhum item cadastrado' if collection.empty?
+end
+
+def search_items(collection)
+  print 'Digite uma palavra para procurar: '
+  term = gets.chomp
+  found_items = collection.filter do |item|
+    item.title.include?(term)
+  end
+  print_items(found_items)
+  puts 'Nenhum item encontrado' if collection.empty?
+end
+
+clear
 puts welcome
 study_items = []
 option = menu
 
-while option != 4
-  if option == 1
+loop do
+  clear
+  case option
+  when 1
     study_items << register_study_item
-  elsif option == 2
-    study_items.each_with_index do |item, index|
-      puts "##{index + 1} - #{item[:title]} - #{item[:category]}"
-    end
-    puts 'Nenhum item cadastrado' if study_items.empty?
-  elsif option == 3
-    print 'Digite uma palavra para procurar: '
-    term = gets.chomp
-    found_items = study_items.filter do |item|
-      item[:title].include? term
-    end
-    puts found_items
-    puts 'Nenhum item encontrado' if found_items.empty?
+  when 2
+    print_items(study_items)
+  when 3
+    search_items(study_items)
+  when 4
+    clear
+    puts 'Obrigado por usar o Diário de Estudos'
+    break
   else
     puts 'Opção inválida'
   end
+  clear_and_wait_keypress
   option = menu
 end
-
-puts 'Obrigado por usar o Diário de Estudos'  
---
+++ aquecimento_iugu/study_item.rb
@@ -0,0 +1,8 @@
+class StudyItem
+    attr_reader :title, :category
+  
+    def initialize(title, category)
+      @title = title
+      @category = category
+    end
+end
